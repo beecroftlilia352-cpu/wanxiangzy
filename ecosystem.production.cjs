f66b270889137bc9925ba6856c73de0bfa8a9372
Gan Jingmeng <ganjmeng@gmail.com>
2026-08-20 00:02:05 +0800

    perf: cap web V8 heap to 384MB to reduce RSS

--- ecosystem.production.cjs
+++ ecosystem.production.cjs
@@ -55,6 +55,7 @@ module.exports = {
       instances: webInstances,
       instance_var: "PM2_INSTANCE_ID",
       max_memory_restart: "1200M",
+      node_args: ["--max-old-space-size=384"],
     },
     {
       ...common,
